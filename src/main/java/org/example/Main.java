6a6579f4a522c6b8f8b04eb6a3e44a1e73b68714
lepri4dw <lepri4dw@gmail.com>
2024-05-06 23:18:36 +0600

    Added Hibernate and model example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import model.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Создание объекта пользователя
+        User user = new User();
+        user.setUsername("John Doe");
+
+        // Создание сессии Hibernate
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+
+        // Сохранение пользователя в базе данных
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Вывод всех пользователей из базы данных
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            List<User> users = session.createQuery("from User", User.class).getResultList();
+            for (User retrievedUser : users) {
+                System.out.println("Retrieved User: " + retrievedUser.getUsername());
+            }
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Закрытие фабрики сессий Hibernate
+        sessionFactory.close();
     }
 }
