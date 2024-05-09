ccaf4626249b2feda3eb3759eb06c02211c38a45
lepri4dw <lepri4dw@gmail.com>
2024-05-09 22:22:11 +0600

    Added Flight model

---
+++ src/main/java/model/Flight.java
@@ -0,0 +1,78 @@
+package model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "flights")
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "flight_number")
+    private String flightNumber;
+
+    @Column(name = "departure_city")
+    private String departureCity;
+
+    @Column(name = "destination_city")
+    private String destinationCity;
+
+    @Column(name = "departure_time")
+    private String departureTime;
+
+    @Column(name = "arrival_time")
+    private String arrivalTime;
+
+    // Конструкторы, геттеры и сеттеры
+
+    public Flight() {
+    }
+
+    public Flight(String flightNumber, String departureCity, String destinationCity, String departureTime, String arrivalTime) {
+        this.flightNumber = flightNumber;
+        this.departureCity = departureCity;
+        this.destinationCity = destinationCity;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDepartureCity() {
+        return departureCity;
+    }
+
+    public void setDepartureCity(String departureCity) {
+        this.departureCity = departureCity;
+    }
+
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    public void setDestinationCity(String destinationCity) {
+        this.destinationCity = destinationCity;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,46 +1,50 @@
 package org.example;
 
+import model.Flight;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import model.User;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Создание объекта пользователя
-        User user = new User();
-        user.setUsername("John Doe");
-
-        // Создание сессии Hibernate
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Flight.class)
                 .buildSessionFactory();
 
-        // Сохранение пользователя в базе данных
-        try (Session session = sessionFactory.getCurrentSession()) {
-            session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Session session = sessionFactory.getCurrentSession();
 
-        // Вывод всех пользователей из базы данных
-        try (Session session = sessionFactory.getCurrentSession()) {
+        try {
             session.beginTransaction();
-            List<User> users = session.createQuery("from User", User.class).getResultList();
-            for (User retrievedUser : users) {
-                System.out.println("Retrieved User: " + retrievedUser.getUsername());
+
+            Flight flight1 = new Flight("SU101", "Moscow", "New York", "10:00", "16:00");
+            Flight flight2 = new Flight("LH202", "Berlin", "London", "12:00", "14:00");
+            Flight flight3 = new Flight("BA303", "Paris", "Tokyo", "14:00", "08:00");
+
+            session.save(flight1);
+            session.save(flight2);
+            session.save(flight3);
+
+            session.getTransaction().commit();
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            List<Flight> flights = session.createQuery("from Flight").getResultList();
+
+            System.out.println("Список рейсов:");
+            for (Flight flight : flights) {
+                System.out.println("Рейс: " + flight.getFlightNumber() +
+                        ", Откуда: " + flight.getDepartureCity() +
+                        ", Куда: " + flight.getDestinationCity() +
+                        ", Время вылета: " + flight.getDepartureTime() +
+                        ", Время прибытия: " + flight.getArrivalTime());
             }
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        // Закрытие фабрики сессий Hibernate
-        sessionFactory.close();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
     }
 }
